6b78a4e48f5332823356c1f24926025f1eadcacb
Wyatt Hoodes <hoodesw@hawaii.edu>
2018-07-03 20:50:26 -1000

    Add comments to the Book Interface

--- src/Book.java
+++ src/Book.java
@@ -2,20 +2,55 @@ import java.util.Calendar;
 
 public interface Book {
 
+    /**
+     * Get the title of the book
+     * 
+     * @param title
+     */
     void setTitle(String title);
 
+    /**
+     * Get the title of the book
+     *
+     * @return String
+     */
     String getTitle();
 
     void setAuthor(String author);
 
+    /**
+     * Get the author of the book
+     *
+     * @return String
+     */
     String getAuthor();
 
+    /**
+     * Get true for checked out, otherwise false
+     *
+     * @return String
+     */
     boolean getCheckedOut();
 
+    /**
+     * True for checked out, otherwise false
+     *
+     * @param checkedOut
+     */
     void setCheckedOut(boolean checkedOut);
 
+    /**
+     * Get the book's due date
+     *
+     * @return Calendar
+     */
     Calendar getDueDate();
 
+    /**
+     * Set the book's due date
+     *
+     * @param dueDate
+     */
     void setDueDate(Calendar dueDate);
 
 }
