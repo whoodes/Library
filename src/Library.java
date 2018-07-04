36ddd42862cdb054a1b95d8d9cb9818412fd4904
Wyatt Hoodes <hoodesw@hawaii.edu>
2018-07-03 21:20:58 -1000

    Add display and search methods for Patrons and Books in the Library class

--- src/Library.java
+++ src/Library.java
@@ -18,6 +18,88 @@ public class Library {
             "9. Display all currently checked out Library books.\n" +
             "10. Exit.";
 
+    /**
+     * Display the current Patrons.
+     * Returns the empty String if no
+     * Patrons are in the system
+     *
+     * @return String
+     */
+    public String displayPatrons(){
+
+        for(Patron p : libraryPatrons){
+            return p.toString();
+        }
+        return "";
+
+    }
+
+    /**
+     * Display the current LibraryBooks.
+     * Returns the empty String if no
+     * LibraryBooks are in the system
+     *
+     * @return String
+     */
+    public String displayLibraryBooks(){
+
+        for(LibraryBook b : libraryShelf){
+            return b.toString();
+        }
+        return "";
+
+    }
+
+    /**
+     * Check each LibraryBook in the system against
+     * a String key.  If there is a match return true,
+     * otherwise return false.
+     *
+     * @param key String
+     * @return boolean
+     */
+    public boolean searchByBook(String key) {
+
+        boolean result = false;
+        for (LibraryBook b : libraryShelf) {
+
+            if (b.getTitle().contains(key) || b.getAuthor().contains(key)) {
+                result = true;
+            }else{
+                continue;
+            }
+
+        }
+
+        return result;
+
+    }
+
+    /**
+     * Check each Patron in the system against
+     * a String key. If there is a match return true,
+     * otherwise return false.
+     *
+     * @param key String
+     * @return boolean
+     */
+    public boolean searchByPatron(String key){
+
+        boolean result = false;
+        for(Patron p : libraryPatrons){
+
+            if(p.getFirstName().contains(key) || p.getLastName().contains(key)){
+                result = true;
+            }else{
+                continue;
+            }
+
+        }
+
+        return result;
+
+    }
+
     public static void main(String[] args){
 
         System.out.println(welcomeMessage + options);
